c22e7a9ee625302eb8a80967c986ebda4fac87d0
simsimhekoon <37258541+simsimhekoon@users.noreply.github.com>
2023-01-29 19:17:16 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+
+const db = require('./models');
+const { Member } = db;
+
+app.use(express.json());
+
+app.get('/api/members', async (req, res) => {
+  const { team } = req.query;
+  if (team) {
+    const teamMembers = await Member.findAll({ where: { team } }); 
+    res.send(teamMembers);
+  } else {
+    const members = await Member.findAll(); 
+    res.send(members);
+  }
+});
+
+app.get('/api/members/:id', async (req, res) => {
+    // const id = req.params.id;
+    const { id } = req.params;
+
+    const member = await Member.findOne({ where: { id } });
+    if(member){
+        res.send(member);
+    } else {
+        res.status(404).send({ message : 'There is no such memeber' });
+    }
+});
+
+app.post('/api/members', async (req, res) => {
+    // console.log(req.body);
+    const newMember = req.body;
+    const member = Member.build(newMember);
+    await member.save();
+    res.send(newMember);
+});
+
+app.put('/api/members/:id', async (req, res) => {
+    const { id } = req.params;
+    const newInfo = req.body;
+    const result = await Member.update(newInfo, { where: { id } });
+
+    if(result[0]) {
+        res.send({ message: `${result[0]} row(s) affected`});
+    }else{
+        res.status(404).send({ message: 'there is no id' });
+    }
+});
+
+app.delete('/api/members/:id', async (req, res) => {
+    const { id } = req.params;
+    const deletedCount = await Member.destroy({ where: { id } });
+    if(deletedCount) {
+        res.send({ message: `${deletedCount} row(s) Deleted!!` });
+    } else {
+        res.status(404).send({ message: 'there is no id' });
+    }
+
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('server is listening...');
+});
---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: "root",
+    password: "Simsim12!",
+    database: "COWORK",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    username: precess.env.DB_USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
+    host: process.env.HOST,
+    dialect: "mysql"
+  }
+}
