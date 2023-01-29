cb43b0ba7b78d5b3990dd42c29966b20b5233993
simsimhekoon <tlfqkfka12@naver.com>
2023-01-29 22:09:03 +0900

    오타 수정
    DB_를 안썼더라... 정신 나갈것같아,,,

--- config/config.js
+++ config/config.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   production: {
     username: process.env.DB_USERNAME,
-    password: process.env.PASSWORD,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB,
     host: process.env.HOST,
     dialect: "mysql"
