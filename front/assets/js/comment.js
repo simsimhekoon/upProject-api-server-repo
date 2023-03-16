c84bba463827df688c77a62d9f4eaa2f927086d2
simsimhekoon <37258541+simsimhekoon@users.noreply.github.com>
2023-03-16 23:39:20 +0900

    댓글 페이징 수정
    댓글 페이징 수정

--- front/assets/js/comment.js
+++ front/assets/js/comment.js
@@ -1,26 +1,46 @@
+let commentPageLength = document.querySelectorAll(".num").length;
+console.log(commentPageLength);
 function changePage(postId, num) {
-  let currentPage = parseInt(event.currentTarget.innerText);
-  location.href = `/post/comment/view/${currentPage}?postId=${postId}&num=${num}`;
+  if(commentPageLength>4){
+    let currentPage = parseInt(event.currentTarget.innerText);
+    location.href = `/post/comment/view/${currentPage}?postId=${postId}&num=${num}`;
+  }
 }
 
 function firstPage(postId, num) {
-  location.href = `/post/comment/view/1?postId=${postId}&num=${num}`;
+  if(commentPageLength>4){
+    location.href = `/post/comment/view/1?postId=${postId}&num=${num}`;
+  }
 }
 
 function prevPage(postId, num) {
-  let currentPage = parseInt(document.querySelector(".on").innerText);
-  if (currentPage != 1) {
-    location.href = `/post/comment/view/${currentPage - 1}?postId=${postId}&num=${num}`;
+  if(commentPageLength>4){
+    let currentPage = parseInt(document.querySelector(".on").innerText);
+    if (currentPage != 1) {
+      location.href = `/post/comment/view/${currentPage - 1}?postId=${postId}&num=${num}`;
+    }
   }
 }
 
 function nextPage(endPage, postId, num) {
-  let currentPage = parseInt(document.querySelector(".on").innerText);
-  if (currentPage != Math.floor(endPage)) {
-    location.href = `/post/comment/view/${currentPage + 1}?postId=${postId}&num=${num}`;
+  if(commentPageLength>4){
+    let currentPage = parseInt(document.querySelector(".on").innerText);
+    if (currentPage != Math.floor(endPage)) {
+      if (endPage == Math.floor(endPage)) {
+        location.href = `/post/comment/view/${currentPage}?postId=${postId}&num=${num}`;
+      } else {
+        location.href = `/post/comment/view/${currentPage + 1}?postId=${postId}&num=${num}`;
+      }
+    }
   }
 }
 
 function lastPage(endPage, postId, num) {
-  location.href = `/post/comment/view/${Math.floor(endPage)}?postId=${postId}&num=${num}`;
+  if(commentPageLength>4){
+    if (endPage == Math.floor(endPage)) {
+      location.href = `/post/comment/view/${Math.floor(endPage) - 1}?postId=${postId}&num=${num}`;
+    } else {
+      location.href = `/post/comment/view/${Math.floor(endPage)}?postId=${postId}&num=${num}`;
+    }
+  }
 }
